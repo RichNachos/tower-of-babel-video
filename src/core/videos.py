2e872f69cf00f64baa54aef605681e4ad244fa16
RichNachos <gkldi20@freeuni.edu.ge>
2025-09-08 21:57:02 +0400

    Add video class

--- src/core/videos.py
+++ src/core/videos.py
@@ -1,6 +1,23 @@
-from dataclasses import dataclass
+from __future__ import annotations
+
+import uuid
+from dataclasses import dataclass, field
+from typing import Protocol
+
+
+@dataclass
+class Video:
+    original_name: str
+
+    id: str = field(default_factory=lambda: str(uuid.uuid4()))
 
 
 @dataclass
 class VideoService:
     pass
+
+
+class VideoRepository(Protocol):
+    def select(self, video_id: str) -> Video: ...
+
+    def insert(self, video: Video) -> None: ...
