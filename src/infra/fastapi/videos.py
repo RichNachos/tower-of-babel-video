7798f998203203bf63fafe8358065aef6675b255
RichNachos <gkldi20@freeuni.edu.ge>
2025-09-08 22:17:21 +0400

    Rename

--- src/infra/fastapi/videos.py
+++ src/infra/fastapi/videos.py
@@ -4,13 +4,13 @@ from fastapi import APIRouter, status
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel, HttpUrl
 
-index_router = APIRouter(tags=["Videos"])
+video_router = APIRouter(tags=["Videos"])
 
 
 class UploadVideo(BaseModel):
     video_url: HttpUrl
 
 
-@index_router.post("/", status_code=status.HTTP_200_OK)
-def upload(request: UploadVideo) -> JSONResponse:  # noqa: ARG001
+@video_router.post("/upload-video", status_code=status.HTTP_200_OK)
+def upload_video(request: UploadVideo) -> JSONResponse:  # noqa: ARG001
     return JSONResponse("")
